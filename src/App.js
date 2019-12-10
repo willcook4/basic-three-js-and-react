7b4c593eed65eeae5966a6e0a30d0d08f9c81331
Will Cook <hello@doubleucook.com>
2019-12-11 11:39:02 +1300

    init setup for three js

--- src/App.js
+++ src/App.js
@@ -2,17 +2,26 @@ import React, { Component } from 'react';
 import * as THREE from 'three';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.mountPointRef = React.createRef()
+  }
+
   componentDidMount() {
     this.sceneSetup();
+    this.addCustomSceneOnjects();
+    this.startAnimationLoop();
   }
 
   sceneSetup = () => {};
 
+  addCustomSceneOnjects = () => {};
+
+  startAnimationLoop = () => {};
+
   render() {
     return (
-      <div>
-        <p>test</p>
-      </div>
+      <div ref={this.mountPointRef} />
     );
   }
 }
