1c1ca1bc94e9c04a5bc0c5b208015958afd977b0
Will Cook <hello@doubleucook.com>
2019-12-11 12:06:14 +1300

    added styled components to make styling easier later and completed the basic scene setup

--- src/App.js
+++ src/App.js
@@ -1,5 +1,11 @@
 import React, { Component } from 'react';
 import * as THREE from 'three';
+import styled from 'styled-components';
+
+const AnimationWrapper = styled.div`
+  /* scene size is controlled by setting container dimensions */
+  width: 250px;
+`
 
 class App extends Component {
   constructor(props) {
@@ -13,7 +19,29 @@ class App extends Component {
     this.startAnimationLoop();
   }
 
-  sceneSetup = () => {};
+  // Setup the scene, camera and renderer
+  sceneSetup = () => {
+    let width = this.mountPointRef.current.clientWidth // window.innerWidth;
+    let height = this.mountPointRef.current.clientHeight // window.innerHeight; 
+
+    // Setup Scene
+    this.scene = new THREE.Scene();
+    // Setup a camera
+    this.camera = new THREE.PerspectiveCamera(
+      75, // fov = field of view
+      width / height, // aspect ratio
+      0.1, // near clipping plane (anything closer than this wont be rendered)
+      1000 // far plane (anything further away than this wont be rendered)
+    );
+    // set a distance from the cube( cube is located at z = 0)
+    this.camera.position.z = 5;
+
+    this.renderer = new THREE.WebGLRenderer(); // instantiate the WebGL renderer
+    this.renderer.setSize( width, height); // set the size of the renderer
+
+    // mount using the react ref
+    this.mountPointRef.current.appendChild( this.renderer.domElement );
+  };
 
   addCustomSceneOnjects = () => {};
 
@@ -21,7 +49,7 @@ class App extends Component {
 
   render() {
     return (
-      <div ref={this.mountPointRef} />
+      <AnimationWrapper ref={this.mountPointRef} />
     );
   }
 }
